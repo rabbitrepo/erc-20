bde3ebe9ffa1d86923680843812f94d02669c88c
rabbitrepo <128143400+rabbitrepo@users.noreply.github.com>
2024-09-03 21:54:14 +0700

    Implemented erc-20 contract & test

--- packages/foundry/contracts/YourContract.sol
+++ packages/foundry/contracts/YourContract.sol
@@ -1,84 +1,113 @@
 //SPDX-License-Identifier: MIT
+
+// Specs:
+// [x] ERC20 Token
+// [x] No need for access control, all methods is accessible by anyone.
+// [x] Public mint, anyone can mint for free, have a capped at 100 tokens
+// [x] One address can only have 1 token. If the address already has a token, minting will fail.
+
 pragma solidity >=0.8.0 <0.9.0;
 
-// Useful for debugging. Remove when deploying to a live network.
-import "forge-std/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+contract YourContract is ERC20 {
+    address public immutable owner;
+    uint256 public constant MAX_SUPPLY = 100;
+    mapping(address => bool) public hasToken;
 
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
-contract YourContract {
-  // State Variables
-  address public immutable owner;
-  string public greeting = "Building Unstoppable Apps!!!";
-  bool public premium = false;
-  uint256 public totalCounter = 0;
-  mapping(address => uint256) public userGreetingCounter;
-
-  // Events: a way to emit log statements from smart contract that can be listened to by external parties
-  event GreetingChange(
-    address indexed greetingSetter,
-    string newGreeting,
-    bool premium,
-    uint256 value
-  );
-
-  // Constructor: Called once on contract deployment
-  // Check packages/foundry/deploy/Deploy.s.sol
-  constructor(address _owner) {
-    owner = _owner;
-  }
-
-  // Modifier: used to define a set of rules that must be met before or after a function is executed
-  // Check the withdraw() function
-  modifier isOwner() {
-    // msg.sender: predefined variable that represents address of the account that called the current function
-    require(msg.sender == owner, "Not the Owner");
-    _;
-  }
-
-  /**
-   * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-   *
-   * @param _newGreeting (string memory) - new greeting to save on the contract
-   */
-  function setGreeting(string memory _newGreeting) public payable {
-    // Print data to the anvil chain console. Remove when deploying to a live network.
-
-    console.logString("Setting new greeting");
-    console.logString(_newGreeting);
-
-    greeting = _newGreeting;
-    totalCounter += 1;
-    userGreetingCounter[msg.sender] += 1;
-
-    // msg.value: built-in global variable that represents the amount of ether sent with the transaction
-    if (msg.value > 0) {
-      premium = true;
-    } else {
-      premium = false;
+    constructor(address _owner) ERC20("Rabbitrepo", "RBRP") {
+        owner = _owner;
     }
 
-    // emit: keyword used to trigger an event
-    emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
-  }
-
-  /**
-   * Function that allows the owner to withdraw all the Ether in the contract
-   * The function can only be called by the owner of the contract as defined by the isOwner modifier
-   */
-  function withdraw() public isOwner {
-    (bool success,) = owner.call{ value: address(this).balance }("");
-    require(success, "Failed to send Ether");
-  }
-
-  /**
-   * Function that allows the contract to receive ETH
-   */
-  receive() external payable { }
+     // Minting is open to anyone and for free, with restrictions
+    function mint(address to) public {
+        require(totalSupply() < MAX_SUPPLY, "Max supply reached");
+        require(!hasToken[to], "Sorry, you can only get 1 token");
+        _mint(to, 1); // Each minting only allows 1 token
+        hasToken[to] = true; // Mark address as having a token
+    }
 }
+
+// pragma solidity >=0.8.0 <0.9.0;
+
+// // Useful for debugging. Remove when deploying to a live network.
+// import "forge-std/console.sol";
+
+// // Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
+// // import "@openzeppelin/contracts/access/Ownable.sol";
+
+// /**
+//  * A smart contract that allows changing a state variable of the contract and tracking the changes
+//  * It also allows the owner to withdraw the Ether in the contract
+//  * @author BuidlGuidl
+//  */
+// contract YourContract {
+//   // State Variables
+//   address public immutable owner;
+//   string public greeting = "Building Unstoppable Apps!!!";
+//   bool public premium = false;
+//   uint256 public totalCounter = 0;
+//   mapping(address => uint256) public userGreetingCounter;
+
+//   // Events: a way to emit log statements from smart contract that can be listened to by external parties
+//   event GreetingChange(
+//     address indexed greetingSetter,
+//     string newGreeting,
+//     bool premium,
+//     uint256 value
+//   );
+
+//   // Constructor: Called once on contract deployment
+//   // Check packages/foundry/deploy/Deploy.s.sol
+//   constructor(address _owner) {
+//     owner = _owner;
+//   }
+
+//   // Modifier: used to define a set of rules that must be met before or after a function is executed
+//   // Check the withdraw() function
+//   modifier isOwner() {
+//     // msg.sender: predefined variable that represents address of the account that called the current function
+//     require(msg.sender == owner, "Not the Owner");
+//     _;
+//   }
+
+//   /**
+//    * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
+//    *
+//    * @param _newGreeting (string memory) - new greeting to save on the contract
+//    */
+//   function setGreeting(string memory _newGreeting) public payable {
+//     // Print data to the anvil chain console. Remove when deploying to a live network.
+
+//     console.logString("Setting new greeting");
+//     console.logString(_newGreeting);
+
+//     greeting = _newGreeting;
+//     totalCounter += 1;
+//     userGreetingCounter[msg.sender] += 1;
+
+//     // msg.value: built-in global variable that represents the amount of ether sent with the transaction
+//     if (msg.value > 0) {
+//       premium = true;
+//     } else {
+//       premium = false;
+//     }
+
+//     // emit: keyword used to trigger an event
+//     emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, msg.value);
+//   }
+
+//   /**
+//    * Function that allows the owner to withdraw all the Ether in the contract
+//    * The function can only be called by the owner of the contract as defined by the isOwner modifier
+//    */
+//   function withdraw() public isOwner {
+//     (bool success,) = owner.call{ value: address(this).balance }("");
+//     require(success, "Failed to send Ether");
+//   }
+
+//   /**
+//    * Function that allows the contract to receive ETH
+//    */
+//   receive() external payable { }
+// }
--- packages/foundry/test/YourContract.t.sol
+++ packages/foundry/test/YourContract.t.sol
@@ -1,28 +1,107 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
 import "../contracts/YourContract.sol";
 
 contract YourContractTest is Test {
-  YourContract public yourContract;
+    YourContract public yourContract;
+    address public owner = address(0x123);
+    address public addr1 = address(0x456);
+    address public addr2 = address(0x789);
 
-  function setUp() public {
-    yourContract = new YourContract(vm.addr(1));
-  }
+    function setUp() public {
+        yourContract = new YourContract(owner);
+    }
 
-  function testMessageOnDeployment() public view {
-    require(
-      keccak256(bytes(yourContract.greeting()))
-        == keccak256("Building Unstoppable Apps!!!")
-    );
-  }
+    function testInitialSetup() public {
+        assertEq(yourContract.owner(), owner);
+        assertEq(yourContract.totalSupply(), 0);
+    }
 
-  function testSetNewMessage() public {
-    yourContract.setGreeting("Learn Scaffold-ETH 2! :)");
-    require(
-      keccak256(bytes(yourContract.greeting()))
-        == keccak256("Learn Scaffold-ETH 2! :)")
-    );
-  }
+    function testMintToken() public {
+        // Initial state
+        assertEq(yourContract.totalSupply(), 0);
+        assertEq(yourContract.balanceOf(addr1), 0);
+        assert(!yourContract.hasToken(addr1));
+
+        // Mint a token to addr1
+        yourContract.mint(addr1);
+
+        // Check that the total supply increased
+        assertEq(yourContract.totalSupply(), 1);
+        assertEq(yourContract.balanceOf(addr1), 1);
+        assert(yourContract.hasToken(addr1));
+
+        // Attempt to mint another token to addr1 should fail
+        vm.expectRevert("Sorry, you can only get 1 token");
+        yourContract.mint(addr1);
+
+        // Mint another token to addr2
+        yourContract.mint(addr2);
+
+        // Check that the total supply increased
+        assertEq(yourContract.totalSupply(), 2);
+        assertEq(yourContract.balanceOf(addr2), 1);
+        assert(yourContract.hasToken(addr2));
+
+        // Attempt to mint a new token after reaching max supply should fail
+        for (uint256 i = 0; i < 98; i++) {
+            address newAddr = address(uint160(i + 1));
+            yourContract.mint(newAddr);
+        }
+        
+        vm.expectRevert("Max supply reached");
+        yourContract.mint(address(0xABC));
+    }
+
+    function testMaxSupply() public {
+        // Mint tokens up to max supply
+        for (uint256 i = 0; i < 100; i++) {
+            address newAddr = address(uint160(i + 1));
+            yourContract.mint(newAddr);
+        }
+        assertEq(yourContract.totalSupply(), 100);
+
+        // Attempt to mint after reaching max supply should fail
+        vm.expectRevert("Max supply reached");
+        yourContract.mint(address(0xDEF));
+    }
+
+    function testCannotMintTwice() public {
+        // Mint to addr1
+        yourContract.mint(addr1);
+
+        // Attempt to mint again to addr1 should fail
+        vm.expectRevert("Sorry, you can only get 1 token");
+        yourContract.mint(addr1);
+    }
 }
+
+// pragma solidity ^0.8.13;
+
+// import "forge-std/Test.sol";
+// import "../contracts/YourContract.sol";
+
+// contract YourContractTest is Test {
+//   YourContract public yourContract;
+
+//   // function setUp() public {
+//   //   yourContract = new YourContract(vm.addr(1));
+//   // }
+
+//   // function testMessageOnDeployment() public view {
+//   //   require(
+//   //     keccak256(bytes(yourContract.greeting()))
+//   //       == keccak256("Building Unstoppable Apps!!!")
+//   //   );
+//   // }
+
+//   // function testSetNewMessage() public {
+//   //   yourContract.setGreeting("Learn Scaffold-ETH 2! :)");
+//   //   require(
+//   //     keccak256(bytes(yourContract.greeting()))
+//   //       == keccak256("Learn Scaffold-ETH 2! :)")
+//   //   );
+//   // }
+// }
